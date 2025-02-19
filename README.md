300104fda6ea1bd1431c4445bebe88478b0ed992
FrostBaer <deakorsi93@gmail.com>
2025-02-19 22:37:00 +0100

    Basic popup & permissions

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 **Color Picker Chrome Extension**
 
-See full [tutorial video](https://www.youtube.com/watch?v=GGi7Brsf7js)
+ - See full [tutorial video](https://www.youtube.com/watch?v=GGi7Brsf7js)
+
+ - Download [cat icon](https://icons8.de/icons/set/cat)
---
+++ service-worker.js
@@ -0,0 +1,14 @@
+//Using the Chrome API 
+//It needs to be enabled in the manifest file
+//event listener will fire when the extension is clicked
+chrome.action.onClicked.addListener(tab => {
+    // chrome.scripting API needs special permissions (scripting)
+    // we also have to define, where the script can run
+    chrome.scripting.executeScript({
+        //what tab do we execute the script on
+        target: {tabId: tab.id},
+        func: () => {
+            alert("Hello from my extension!");
+        }
+    });
+});
